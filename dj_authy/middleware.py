f6524bce2dab5930a7837ce7fbc207f62ee58f34
Mark Garro <garromark@gmail.com>
2015-07-10 16:30:43 -0500

    Allow middleware to operate before authy profile created

--- dj_authy/middleware.py
+++ dj_authy/middleware.py
@@ -26,8 +26,8 @@ class AuthyAuthenticationRequiredMiddleware(object):
             # only if we dont already have it
             #
             if request.session.get(AUTHY_SESSION_KEY, None) in [None, False]:
-
-                profile_data = request.user.profile.data
+                profile = request.user.auth_profile
+                profile_data = profile.data
 
                 # if the required key is present in the profile data and is set to True
                 if AUTHY_IS_REQUIRED_KEY in profile_data and profile_data[AUTHY_IS_REQUIRED_KEY] is True:
